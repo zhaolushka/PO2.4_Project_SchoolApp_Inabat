214843d0e1ed484a5dac4979706b71d1d88f79e6
zhaolushka <kairakbaiinabat09@gmail.com>
2026-05-14 21:36:23 +0500

    Add MAUI project files

---
+++ SchoolApp/SchoolApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SchoolApp/SchoolApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace SchoolApp;
+
+public partial class MainPage : ContentPage
+{
+    private int _count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnTapClicked(object sender, EventArgs e)
+    {
+        _count++;
+        StatusLabel.Text = $"Button tapped {_count} time(s).";
+    }
+}
