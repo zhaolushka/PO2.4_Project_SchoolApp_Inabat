68cda77807cd6080c20773aaba1b320c81d274a0
zhaolushka <kairakbaiinabat09@gmail.com>
2026-05-28 18:24:49 +0500

    L15: data binding on MainPage

--- SchoolApp/SchoolApp/MainPage.xaml.cs
+++ SchoolApp/SchoolApp/MainPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿namespace SchoolApp;
 
+using SchoolApp.ViewModels;
+
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+
+        BindingContext = new Person();
     }
 
     private async void OnOpenStudentsClicked(object sender, EventArgs e)
---
+++ SchoolApp/SchoolApp/ViewModels/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace SchoolApp.ViewModels;
+
+public class Person
+{
+    public string Name { get; set; } = "Aida";
+    public string City { get; set; } = "Almaty";
+}
